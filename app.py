169d35d14808f2224b9ea944ac49a832405cbcf7
RobertoChiosa <roberto.chiosa@gmail.com>
2022-02-09 09:17:46 +0100

    added json name

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def home():
 @app.route('/process', methods=['POST'])
 def detect_intent():
     # create a new session
-    credentials = service_account.Credentials.from_service_account_file("client-secret.json")
+    credentials = service_account.Credentials.from_service_account_file("homie-key.json")
     session_client = dialogflow.SessionsClient(credentials=credentials)
 
     # replace the project id, since it is fake
     # 123456 is the session id, which should not be changed within an entire conversation with a single user
-    session = session_client.session_path("weather-lvbjmw", 123456)
+    session = session_client.session_path("homie-l9jt", 123456)
 
     # prepare a new query input with the inner text, taken from the POST request
     text_input = dialogflow.types.TextInput(text=request.form["message"], language_code="en-US")
